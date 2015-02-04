74dc22106d5b8c4178e6dc9a2b0e9c332b652a39
moznion <moznion@gmail.com>
2015-02-04 14:24:41 +0900

    Add encryption function

---
+++ encrypter.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+
+	"github.com/ianmcmahon/encoding_ssh"
+)
+
+func EncryptStringBySshRsaPublicKey(sshRsaPubkey string, msg string) ([]byte, error) {
+	pubkey, err := ssh.DecodePublicKey(sshRsaPubkey)
+	if err != nil {
+		return make([]byte, 0), err
+	}
+
+	enc, err := rsa.EncryptPKCS1v15(rand.Reader, pubkey.(*rsa.PublicKey), []byte(msg))
+	if err != nil {
+		return make([]byte, 0), err
+	}
+
+	return enc, nil
+}
