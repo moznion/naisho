dff2e6e0af0cb4d1ce52c5ee7a83b5952534c47c
moznion <moznion@gmail.com>
2015-02-04 14:52:14 +0900

    Return with error

--- github_info.go
+++ github_info.go
@@ -2,58 +2,54 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"regexp"
 )
 
-func FetchPublicKeyByUserName(userName string) string {
+func FetchPublicKeyByUserName(userName string) (string, error) {
 	res, err := http.Get("https://github.com/" + userName + ".keys")
 	if err != nil {
-		// TODO
-		return ""
+		return "", err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		// TODO
-		return ""
+		return "", errors.New(fmt.Sprintf("Failed HTTP request: %d", res.StatusCode))
 	}
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		// TODO
-		return ""
+		return "", err
 	}
 
 	r := regexp.MustCompile("\r?\n")
 	lines := r.Split(string(body), -1)
-	return lines[0]
+	return lines[0], nil
 }
 
 type users struct {
 	Email string `json:"email"`
 }
 
-func FetchEmailAddressByUserName(userName string) string {
+func FetchEmailAddressByUserName(userName string) (string, error) {
 	res, err := http.Get("https://api.github.com/users/" + userName)
 	if err != nil {
-		// TODO
-		return ""
+		return "", err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		// TODO
-		return ""
+		return "", errors.New(fmt.Sprintf("Failed HTTP request: %d", res.StatusCode))
 	}
 
 	userInfo := new(users)
 	err = json.NewDecoder(res.Body).Decode(userInfo)
 	if err != nil {
-		// TODO
-		return ""
+		return "", err
 	}
 
-	return userInfo.Email
+	return userInfo.Email, nil
 }
--- github_info_test.go
+++ github_info_test.go
@@ -3,29 +3,30 @@ package main
 import "testing"
 
 func TestFetchPublicKeyThatExists(t *testing.T) {
-	pubkey := FetchPublicKeyByUserName("moznion")
-	if pubkey == "" {
+	pubkey, err := FetchPublicKeyByUserName("moznion")
+	if err != nil || pubkey == "" {
 		t.Error("Failed to fetch public key from GitHub")
 	}
 }
 
 func TestFetchPublicKeyOfNotExistsUser(t *testing.T) {
-	pubkey := FetchPublicKeyByUserName("not_exist_asdfasdf")
-	if pubkey != "" {
-		t.Error("Something received!")
+	_, err := FetchPublicKeyByUserName("not_exist_asdfasdf")
+
+	if err == nil {
+		t.Error("Failed to fetch public key from GitHub")
 	}
 }
 
 func TestFetchEmailAddressThatExists(t *testing.T) {
-	email := FetchEmailAddressByUserName("moznion")
-	if email != "moznion@gmail.com" {
+	email, err := FetchEmailAddressByUserName("moznion")
+	if err != nil || email != "moznion@gmail.com" {
 		t.Error("Failed to fetch users email")
 	}
 }
 
 func TestFetchEmailAddressOfNotExistsUser(t *testing.T) {
-	email := FetchEmailAddressByUserName("not_exist_asdfasdf")
-	if email != "" {
+	_, err := FetchEmailAddressByUserName("not_exist_asdfasdf")
+	if err == nil {
 		t.Error("Something received!")
 	}
 }
